4ab4a6bf730581a9def133f03dba45e178acd9d8
qqqxing <qqqxing@gmail.com>
2012-10-23 14:26:38 +0800

    nothing

--- addnewseries.php
+++ addnewseries.php
@@ -1,58 +1,62 @@
 <?php
 
-	require_once 'config.php';
-	
-	//TODO check session
-	
-	$name_en = "";
-	$name_ch = "";
-	$show_date = "";
-	$show_state = "";
-	$info = "";
-	$img_src = "";
-	
-	if (isset($_POST['nameen'])){
-		$name_en = $_POST['nameen'];
-	}else{
-		die("English name is needed.");
-	}
-	
-	
-	if (isset($_POST['namech'])){
-		$name_ch = $_POST['namech'];
-	}
-	
-	if (isset($_POST['showdate'])){
-		$show_date = $_POST['showdate'];
-	}
-	if (isset($_POST['showstate'])){
-		$show_state = $_POST['showstate'];
-	}
-	if (isset($_POST['info'])){
-		$info = $_POST['info'];
-	}
-	if (isset($_POST['imgsrc'])){
-		$img_src = $_POST['imgsrc'];
-	}
+require_once 'config.php';
 
-	$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-	if (!$mysqli){
-		die("Cannot connect to db.\n");
-	}
-	$query = "insert into series(name_en,name_ch,show_date,show_state,info,img_src) values(?,?,?,?,?,?)";
-	
-	$bind_result = $mysqli->prepare($query);
-	$bind_result->bind_param("ssssss", $name_en,$name_ch,$show_date,$show_state,$info,$img_src);
-	
-	$bind_result->execute();
-	$bind_result->close();
-	$mysqli->close();
-	
-	echo "Add done.\n";
-	
-	
-	
-	
-	
-	
+//TODO check session
+
+$name_en = "";
+$name_ch = "";
+$show_date = "";
+$show_state = "";
+$info = "";
+$img_src = "";
+
+
+
+if (isset($_POST['nameen'])){
+	$name_en = $_POST['nameen'];
+}else{
+	die("English name is needed.");
+}
+
+
+if (isset($_POST['namech'])){
+	$name_ch = $_POST['namech'];
+}
+
+if (isset($_POST['showdate'])){
+	$show_date = $_POST['showdate'];
+}
+if (isset($_POST['showstate'])){
+	$show_state = $_POST['showstate'];
+}
+if (isset($_POST['info'])){
+	$info = $_POST['info'];
+}
+if (isset($_POST['imgsrc'])){
+	$img_src = $_POST['imgsrc'];
+}
+
+$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if (!$mysqli){
+	die("Cannot connect to db.\n");
+}
+
+print "connected.\n";
+
+
+$query = "insert into series(name_en,name_ch,show_date,show_state,info,img_src) values(?,?,?,?,?,?)";
+
+$bind_result = $mysqli->prepare($query);
+$bind_result->bind_param("ssssss", $name_en,$name_ch,$show_date,$show_state,$info,$img_src);
+
+$bind_result->execute();
+$bind_result->close();
+$mysqli->close();
+
+echo "Add done.\n";
+
+
+
+
 ?>
---
+++ meijuexpress.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: meijuexpress
+-- ------------------------------------------------------
+-- Server version	5.1.63-0ubuntu0.11.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `series`
+--
+
+DROP TABLE IF EXISTS `series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_en` varchar(255) NOT NULL,
+  `name_ch` varchar(255) NOT NULL,
+  `show_date` varchar(255) DEFAULT NULL,
+  `show_state` varchar(255) DEFAULT NULL,
+  `info` text,
+  `img_src` varchar(255) DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `series`
+--
+
+LOCK TABLES `series` WRITE;
+/*!40000 ALTER TABLE `series` DISABLE KEYS */;
+/*!40000 ALTER TABLE `series` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-10-23 14:25:24
